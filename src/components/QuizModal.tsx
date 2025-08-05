083954cbcba2a4c78666214af7908eb0a95332b2
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-08-05 14:08:47 +0500

    Move close button to top right corner of modal

--- src/components/QuizModal.tsx
+++ src/components/QuizModal.tsx
@@ -207,7 +207,7 @@ const QuizModal: React.FC<QuizModalProps> = ({ open, onClose }) => {
         <div className="p-6">
           <button
             onClick={onClose}
-            className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+            className="absolute top-6 right-6 text-gray-500 hover:text-gray-700 z-10"
           >
             <X size={24} />
           </button>
