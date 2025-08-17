ba3d458e7dd6e9f1710829286c8ff69be435db10
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-08-17 17:58:30 +0500

    Updated AdminQuiz.tsx

--- src/components/admin/AdminQuiz.tsx
+++ src/components/admin/AdminQuiz.tsx
@@ -232,7 +232,7 @@ const AdminQuiz = () => {
                      }}
                     />
                    <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-transparent to-transparent rounded flex items-end">
-                      <p className="text-teal-500 text-xs p-2 truncate font-medium">
+                      <p className="bg-green-100 text-green-800 text-xs p-2 truncate font-medium">
                         {option.option_label}
                       </p>
                     </div>
