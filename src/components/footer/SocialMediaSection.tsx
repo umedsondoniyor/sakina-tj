93dcd36a53ba6142d60e27a92841edd66ea06b98
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-08-18 14:54:17 +0500

    Updated SocialMediaSection.tsx

--- src/components/footer/SocialMediaSection.tsx
+++ src/components/footer/SocialMediaSection.tsx
@@ -6,7 +6,7 @@ const SocialMediaSection = () => {
     <div>
       <p className="text-sm text-gray-600 mb-2">Следите за новостями</p>
       <div className="flex space-x-3">
-        <a href="https://www.instagram.com/sakina.tj?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="text-gray-400 hover:text-gray-600">
+        <a href="https://www.instagram.com/sakina.tj?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="text-gray-400 hover:text-gray-600" target="_blank">
           <svg className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
             <path d="M15.684 0H8.316C1.592 0 0 1.592 0 8.316v7.368C0 22.408 1.592 24 8.316 24h7.368C22.408 24 24 22.408 24 15.684V8.316C24 1.592 22.408 0 15.684 0zm-3.692 16.5a4.5 4.5 0 110-9 4.5 4.5 0 010 9zm4.95-8.15a1.05 1.05 0 110-2.1 1.05 1.05 0 010 2.1z"/>
           </svg>
