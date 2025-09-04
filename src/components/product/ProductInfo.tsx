deb9d02d019beaafb5e059e8fa773d71548d3b29
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-09-04 21:23:21 +0500

    Updated ProductInfo.tsx

--- src/components/product/ProductInfo.tsx
+++ src/components/product/ProductInfo.tsx
@@ -259,7 +259,7 @@ const ProductInfo: React.FC<ProductInfoProps> = ({
           <Truck className="w-5 h-5 text-gray-600" />
           <div>
             <div>Доставка на дом</div>
-            <div>0 с. в Душанбе</div>
+            <div>Бесплатно в Душанбе</div>
           </div>
         </div>
         <div className="flex items-start space-x-2">
