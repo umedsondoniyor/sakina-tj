e8d4a65fb7aeb839ceee0b5378f8772a48f2bc8d
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-09-10 13:43:51 +0500

    Updated AdminAbout.tsx

--- src/components/admin/AdminAbout.tsx
+++ src/components/admin/AdminAbout.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Plus, Edit2, Trash2, Save, X, Upload, Image as ImageIcon } from 'lucide-react';
 import { useAboutData } from '../../hooks/useAboutData';
-import { aboutApi } from '../../lib/aboutApi';
+import aboutApi  from '../../lib/aboutApi';
 import { AboutSettings, AboutStat, AboutValue, AboutTimelineItem, AboutTeamMember } from '../../lib/types';
 import { validateAboutSettings, validateAboutStat, validateAboutValue, validateAboutTimelineItem, validateAboutTeamMember } from '../../lib/validation';
-import  AboutSettingsForm  from './forms/AboutSettingsForm';
-import  ImageUploadField  from './ImageUploadField';
+import AboutSettingsForm  from './forms/AboutSettingsForm';
+import ImageUploadField  from './ImageUploadField';
 
 const AdminAbout: React.FC = () => {
   const { data, loading, error, refetch } = useAboutData();
