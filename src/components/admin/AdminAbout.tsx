c0880f8127d151a7109dfa2230abf0e3393691f8
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-09-10 13:42:45 +0500

    Updated AdminAbout.tsx

--- src/components/admin/AdminAbout.tsx
+++ src/components/admin/AdminAbout.tsx
@@ -4,7 +4,7 @@ import { useAboutData } from '../../hooks/useAboutData';
 import { aboutApi } from '../../lib/aboutApi';
 import { AboutSettings, AboutStat, AboutValue, AboutTimelineItem, AboutTeamMember } from '../../lib/types';
 import { validateAboutSettings, validateAboutStat, validateAboutValue, validateAboutTimelineItem, validateAboutTeamMember } from '../../lib/validation';
-import { AboutSettingsForm } from './forms/AboutSettingsForm';
+import  AboutSettingsForm  from './forms/AboutSettingsForm';
 import { ImageUploadField } from './ImageUploadField';
 
 const AdminAbout: React.FC = () => {
