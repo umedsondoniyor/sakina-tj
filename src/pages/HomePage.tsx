d943001d23098be1867880d0be12989528162a5a
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-09-28 15:01:37 +0500

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,21 @@
 // src/pages/HomePage.tsx
 import React from 'react';
-import HeroCarousel from '@/components/HeroCarousel';
-import CategoryGrid from '@/components/CategoryGrid';
-import ProductPickers from '@/components/ProductPickers';
-import Promotions from '@/components/Promotions';
-import AskonaClub from '@/components/AskonaClub';
-import BestSellers from '@/components/BestSellers';
-import CustomerReviews from '@/components/CustomerReviews';
-import RecommendedProducts from '@/components/RecommendedProducts';
-import ProductionStats from '@/components/ProductionStats';
-import ManufacturingProcess from '@/components/ManufacturingProcess';
-import SleepClubBlog from '@/components/SleepClubBlog';
-import AboutUs from '@/components/AboutUs';
-import Features from '@/components/Features';
-import ContactSection from '@/components/ContactSection';
-import NotificationAlert from '@/components/NotificationAlert';
-import Benefits from '@/components/Benefits';
+import HeroCarousel from '../components/HeroCarousel';
+import CategoryGrid from '../components/CategoryGrid';
+import ProductPickers from '../components/ProductPickers';
+import Promotions from '../components/Promotions';
+import AskonaClub from '../components/AskonaClub';
+import BestSellers from '../components/BestSellers';
+import CustomerReviews from '../components/CustomerReviews';
+import RecommendedProducts from '../components/RecommendedProducts';
+import ProductionStats from '../components/ProductionStats';
+import ManufacturingProcess from '../components/ManufacturingProcess';
+import SleepClubBlog from '../components/SleepClubBlog';
+import AboutUs from '../components/AboutUs';
+import Features from '../components/Features';
+import ContactSection from '../components/ContactSection';
+import NotificationAlert from '../components/NotificationAlert';
+import Benefits from '../components/Benefits';
 
 const HomePage: React.FC = () => {
   return (
