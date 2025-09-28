5326a56ab4f1cb2472a56767e9796a95b87d978c
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-09-28 15:09:41 +0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { CartProvider } from './contexts/CartContext';
 import CartModal from './components/CartModal';
+import ScrollToTop from './components/ScrollToTop';
 
 // Layouts
 import PublicLayout from './layouts/PublicLayout';
