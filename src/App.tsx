c51dc33c61954418a36f709b4993c7ffb70319d0
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-10-13 10:45:04 +0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import PublicLayout from './layouts/PublicLayout';
 import AdminLayout from './layouts/AdminLayout';
 
 // Public pages
-import HomePage from './pages/HomePage';
 import ProductsPage from './components/ProductsPage';
 import ProductPage from './components/ProductPage';
 import Mattresses from './components/Mattresses';
