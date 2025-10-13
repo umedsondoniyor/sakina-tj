bf4fca99a717676081d707a64b3e9b2594e40e36
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-10-13 10:26:14 +0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 });
+
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
