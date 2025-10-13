95836b246ddf1d5674d1f4e21cdf0c738aa2fcda
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-10-13 09:53:22 +0500

    Updated AdminLayout.tsx

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import AdminNavigation from '@/components/admin/AdminNavigation';
 
-export default function AdminLayout() {
+const AdminLayout: React.FC = () => {
   return (
+    <>
+      {/* Optional: skip link for accessibility */}
     <div className="flex">
       <AdminNavigation />
       <main className="flex-1 p-6 bg-gray-50 min-h-screen">
         <Outlet />
       </main>
     </div>
+    </>
   );
-}
+};
+
+export default PublicLayout;
