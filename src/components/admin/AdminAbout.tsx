b51cb898c244f32c05c2e6aa1372065af3c91fca
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-10-21 15:48:23 +0500

    Updated AdminAbout.tsx

--- src/components/admin/AdminAbout.tsx
+++ src/components/admin/AdminAbout.tsx
@@ -14,6 +14,7 @@ import {
  * - about_team: id uuid pk, name text, position text, description text, image_url text, order int
  */
 
+
 type AboutSettings = {
   id: string;
   hero_title: string | null;
