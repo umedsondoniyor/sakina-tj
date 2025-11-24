314fdfcadfbb1600724cb6b7573b013c42ebc41e
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2025-11-24 09:57:35 +0500

    Updated QuickFilters.tsx

--- src/components/products/QuickFilters.tsx
+++ src/components/products/QuickFilters.tsx
@@ -30,13 +30,13 @@ const QuickFilters: React.FC<QuickFiltersProps> = ({
       <div className="overflow-x-auto scrollbar-hide -mx-4 px-4">
         <div className="flex space-x-3 min-w-max">
           {/* Online mattress wizard */}
-          <button
+          <span
             type="button"
-            onClick={onOpenMattressWizard}
+            {/* onClick={onOpenMattressWizard} */}
             className="flex-none px-4 py-2 bg-yellow-100 rounded-full text-sm whitespace-nowrap hover:bg-yellow-200"
           >
             Онлайн-подбор матраса
-          </button>
+          </span>
 
           {/* Size quick filters */}
           {QUICK_SIZES.map((size) => {
